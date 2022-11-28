9ae49898e62473901e0449765565913fae3f0cb3
Tom Johnston <thomas.johnston@softwire.com>
2022-11-28 12:00:41 +0000

    2019/3/1

--- 2019/Day03/README.md
+++ 2019/Day03/README.md
@@ -51,3 +51,39 @@ U98,R91,D20,R16,D67,R40,U7,R15,U6,R7</code> = distance <code>135</code>
 <em>What is the Manhattan distance</em> from the central port to the closest intersection?
 
 
+## --- Part Two ---
+It turns out that this circuit is very timing-sensitive; you actually need to <em>minimize the signal delay</em>.
+
+To do this, calculate the <em>number of steps</em> each wire takes to reach each intersection; choose the intersection where the <em>sum of both wires' steps</em> is lowest. If a wire visits a position on the grid multiple times, use the steps value from the <em>first</em> time it visits that position when calculating the total value of a specific intersection.
+
+The number of steps a wire takes is the total number of grid squares the wire has entered to get to that location, including the intersection being considered. Again consider the example from above:
+
+<pre>
+<code>...........
+.+-----+...
+.|.....|...
+.|..+--X-+.
+.|..|..|.|.
+.|.-X--+.|.
+.|..|....|.
+.|.......|.
+.o-------+.
+...........
+</code>
+</pre>
+
+In the above example, the intersection closest to the central port is reached after <code>8+5+5+2 = <em>20</em></code> steps by the first wire and <code>7+6+4+3 = <em>20</em></code> steps by the second wire for a total of <code>20+20 = <em>40</em></code> steps.
+
+However, the top-right intersection is better: the first wire takes only <code>8+5+2 = <em>15</em></code> and the second wire takes only <code>7+6+2 = <em>15</em></code>, a total of <code>15+15 = <em>30</em></code> steps.
+
+Here are the best steps for the extra examples from above:
+
+
+ - <code>R75,D30,R83,U83,L12,D49,R71,U7,L72
+U62,R66,U55,R34,D71,R55,D58,R83</code> = <code>610</code> steps
+ - <code>R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51
+U98,R91,D20,R16,D67,R40,U7,R15,U6,R7</code> = <code>410</code> steps
+
+<em>What is the fewest combined steps the wires must take to reach an intersection?</em>
+
+
--- 2019/Day03/Solution.cs
+++ 2019/Day03/Solution.cs
@@ -2,19 +2,83 @@ using System;
 using System.Collections.Generic;
 using System.Collections.Immutable;
 using System.Linq;
+using System.Runtime.InteropServices.JavaScript;
 using System.Text.RegularExpressions;
 using System.Text;
+using AdventOfCode.Y2021.Day02;
 
 namespace AdventOfCode.Y2019.Day03;
 
-[ProblemName("Crossed Wires")]      
-class Solution : Solver {
+[ProblemName("Crossed Wires")]
+class Solution : Solver
+{
+    public object PartOne(string input)
+    {
+        var instructions = from wire in input.Split('\n') select ParseWire(wire);
+        var wirePositions = (from instruction in instructions select GetWirePositions(instruction));
+        var intersections = new HashSet<Coord>();
+        wirePositions.Aggregate(new HashSet<Coord>(),
+            (occupiedPositions, currWirePos) =>
+            {
+                var newIntersections = occupiedPositions.ToHashSet();
+                newIntersections.IntersectWith(currWirePos);
+                occupiedPositions.UnionWith(currWirePos);
+                intersections.UnionWith(newIntersections);
+                return occupiedPositions;
+            });
+        foreach (var coord in intersections.ToList())
+        {
+            Console.WriteLine($"{coord.x},{coord.y}");
+        }
 
-    public object PartOne(string input) {
+        return intersections.Aggregate(99999999, (min, curr) =>
+            min > GetManhattanDistance(curr) ? GetManhattanDistance(curr) : min
+        );
+    }
+
+    public object PartTwo(string input)
+    {
         return 0;
     }
 
-    public object PartTwo(string input) {
-        return 0;
+    int GetManhattanDistance(Coord coord) => Math.Abs(coord.x) + Math.Abs(coord.y);
+
+    HashSet<Coord> GetWirePositions(IEnumerable<Instruction> instructions)
+    {
+        var wire = instructions.Aggregate(new Wire(new HashSet<Coord>(), new Coord(0, 0)), (state, step) =>
+        {
+            var end = state.end;
+            var distance = step.distance;
+            while (distance > 0)
+            {
+                end = MoveEnd(end, step.direction);
+                state.positions.Add(end);
+                distance -= 1;
+            }
+
+            return state with { end = end };
+        });
+        return wire.positions;
+    }
+
+    Coord MoveEnd(Coord end, char dir) => dir switch
+    {
+        'R' => new Coord(end.x + 1, end.y),
+        'D' => new Coord(end.x, end.y - 1),
+        'U' => new Coord(end.x, end.y + 1),
+        'L' => new Coord(end.x - 1, end.y),
+        _ => throw new Exception(),
+    };
+
+    IEnumerable<Instruction> ParseWire(string wire)
+    {
+        var strArr = wire.Split(',');
+        return from str in strArr select new Instruction(str[0], int.Parse(str.Substring(1)));
     }
 }
+
+record Wire(HashSet<Coord> positions, Coord end);
+
+record Coord(int x, int y);
+
+record Instruction(char direction, int distance);
