3d12799456e9658cfe8bfeb8abffe61d5eb5a4e7
Tom Johnston <thomas.johnston@softwire.com>
2022-12-01 10:41:30 +0000

    finish day 1

--- 2022/Day01/Solution.cs
+++ 2022/Day01/Solution.cs
@@ -7,14 +7,30 @@ using System.Text;
 
 namespace AdventOfCode.Y2022.Day01;
 
-[ProblemName("Calorie Counting")]      
-class Solution : Solver {
-
-    public object PartOne(string input) {
-        return 0;
+[ProblemName("Calorie Counting")]
+class Solution : Solver
+{
+    public object PartOne(string input)
+    {
+        var foodPerElf = ParseInput(input);
+        var calPerElf = foodPerElf.Select(elf => elf.Aggregate(0, (total, item) => total + item));
+        var max = calPerElf.Aggregate(0, (max, curr) => curr < max ? max : curr);
+        return max;
     }
 
-    public object PartTwo(string input) {
-        return 0;
+    public object PartTwo(string input)
+    {
+        var foodPerElf = ParseInput(input);
+        var calPerElf = foodPerElf.Select(elf => elf.Aggregate(0, (total, item) => total + item));
+        var max = calPerElf.Aggregate(0, (max, curr) => curr < max ? max : curr);
+        var second = calPerElf.Where(cal => cal != max).Aggregate(0, (max, curr) => curr < max ? max : curr);
+        var third = calPerElf.Where(cal => cal != second && cal != max).Aggregate(0, (max, curr) => curr < max ? max : curr);
+        return max + second + third;
+    }
+
+    IEnumerable<IEnumerable<int>> ParseInput(string input)
+    {
+        var r = input.Split("\n\n").Select((elf) => elf.Split('\n').Select(food => int.Parse(food)));
+        return r;
     }
 }
