7924edc010281d92211d9275c21f3f11eeb8b1e7
Tom Johnston <thomas.johnston@softwire.com>
2022-12-03 13:19:05 +0000

    part 1

--- 2022/Day03/Solution.cs
+++ 2022/Day03/Solution.cs
@@ -7,14 +7,37 @@ using System.Text;
 
 namespace AdventOfCode.Y2022.Day03;
 
-[ProblemName("Rucksack Reorganization")]      
-class Solution : Solver {
+[ProblemName("Rucksack Reorganization")]
+class Solution : Solver
+{
+    public object PartOne(string input)
+    {
+        var backpacks = Read(input);
+        var priorities = from b in backpacks select GetPriority(FindMatch(b));
+        return priorities.Sum();
+    }
 
-    public object PartOne(string input) {
+    public object PartTwo(string input)
+    {
         return 0;
     }
 
-    public object PartTwo(string input) {
-        return 0;
+    int GetPriority(char letter)
+    {
+        var adjustedCode = (int)letter - 96;
+        return adjustedCode > 0 ? adjustedCode : (adjustedCode + 31 + 27);
     }
+
+    char FindMatch((string, string) backpack)
+    {
+        var (a, b) = backpack;
+        var intersection = a.Intersect(b).ToList();
+        return intersection[0];
+    }
+
+    IEnumerable<(string, string)> Read(string input)
+        =>
+            from backback in input.Split('\n')
+            let size = backback.Length
+            select (backback.Substring(0, size / 2), backback.Substring(size / 2));
 }
