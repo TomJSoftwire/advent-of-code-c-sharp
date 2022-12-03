5768362cc2f2f994df410da9c1d0ce35d1ece61a
Tom Johnston <thomas.johnston@softwire.com>
2022-12-03 13:35:21 +0000

    part 2

--- 2022/Day03/Solution.cs
+++ 2022/Day03/Solution.cs
@@ -4,6 +4,7 @@ using System.Collections.Immutable;
 using System.Linq;
 using System.Text.RegularExpressions;
 using System.Text;
+using Microsoft.VisualBasic;
 
 namespace AdventOfCode.Y2022.Day03;
 
@@ -12,14 +13,23 @@ class Solution : Solver
 {
     public object PartOne(string input)
     {
-        var backpacks = Read(input);
+        var backpacks = Read1(input);
         var priorities = from b in backpacks select GetPriority(FindMatch(b));
         return priorities.Sum();
     }
 
     public object PartTwo(string input)
     {
-        return 0;
+        var bags = input.Split('\n').ToList();
+        var total = 0;
+        for (var i = 0; i < bags.Count; i += 3)
+        {
+            var group = bags.GetRange(i, 3);
+            total += GetPriority(GroupMatches(group));
+        }
+
+        return total;
+        
     }
 
     int GetPriority(char letter)
@@ -28,6 +38,10 @@ class Solution : Solver
         return adjustedCode > 0 ? adjustedCode : (adjustedCode + 31 + 27);
     }
 
+    char GroupMatches(IEnumerable<IEnumerable<char>> group)
+        => group.Aggregate(group.First(), (matches, bag) => matches.Intersect(bag)).ToList()[0];
+
+
     char FindMatch((string, string) backpack)
     {
         var (a, b) = backpack;
@@ -35,7 +49,7 @@ class Solution : Solver
         return intersection[0];
     }
 
-    IEnumerable<(string, string)> Read(string input)
+    IEnumerable<(string, string)> Read1(string input)
         =>
             from backback in input.Split('\n')
             let size = backback.Length
