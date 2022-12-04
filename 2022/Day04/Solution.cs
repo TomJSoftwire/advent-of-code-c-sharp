4a4c2027658096c3d2a7f1a8183e17c8b3db1f79
Tom Johnston <thomas.johnston@softwire.com>
2022-12-04 12:39:07 +0000

    improve day 4

--- 2022/Day04/Solution.cs
+++ 2022/Day04/Solution.cs
@@ -1,60 +1,45 @@
 using System;
 using System.Collections.Generic;
 using System.Collections.Immutable;
+using System.Data.Common;
 using System.Linq;
 using System.Text.RegularExpressions;
 using System.Text;
+using AngleSharp.Html.Dom.Events;
 
 namespace AdventOfCode.Y2022.Day04;
 
 [ProblemName("Camp Cleanup")]
 class Solution : Solver
 {
-    public object PartOne(string input)
-    {
-        var groups = GetGroupRanges(input);
-        return groups.Select(g =>
-        {
-            var gList = g.ToArray();
-            if (IsRangeSubsetOfRange(gList[0], gList[1]) || IsRangeSubsetOfRange(gList[1], gList[0]))
-            {
-                return 1;
-            }
+    public object PartOne(string input) =>
+        CountGroupsByCondition((gList) =>
+                IsRangeSubsetOfRange(gList[0], gList[1])
+                || IsRangeSubsetOfRange(gList[1], gList[0])
+            , input);
 
-            return 0;
-        }).Sum();
-    }
 
-    public object PartTwo(string input)
-    {
-        var groups = GetGroupRanges(input);
-        return groups.Select(g =>
-        {
-            var gList = g.ToArray();
-            if (DoRangesOverlap(gList[0], gList[1]))
-            {
-                return 1;
-            }
+    public object PartTwo(string input) =>
+            CountGroupsByCondition((gList) => DoRangesOverlap(gList[0], gList[1]), input);
 
-            return 0;
-        }).Sum();
-    }
+    int CountGroupsByCondition(Func<SectionRange[], bool> testCondition, string input)
+        => (from g in GetGroupRanges(input)
+            let gList = g.ToArray()
+            where testCondition(gList)
+            select g).Count();
 
     bool IsRangeSubsetOfRange(SectionRange parentRange, SectionRange childRange) =>
         parentRange.max >= childRange.max && parentRange.min <= childRange.min;
 
     bool DoRangesOverlap(SectionRange rangeA, SectionRange rangeB) =>
-        rangeA.max >= rangeB.min && rangeA.min <= rangeB.max
-        || rangeB.max >= rangeA.min && rangeB.min <= rangeA.max;
+        rangeA.max >= rangeB.min && rangeA.min <= rangeB.max;
 
     IEnumerable<IEnumerable<SectionRange>> GetGroupRanges(string input) =>
-        input.Split('\n')
-            .Select(group => group.Split(',')
-                .Select(rangeString =>
-                {
-                    var range = rangeString.Split('-').Select(int.Parse).ToList();
-                    return new SectionRange(range[0], range[1]);
-                }));
+        from g in input.Split('\n')
+        select (from elf in g.Split(',')
+                let range = elf.Split('-').Select(int.Parse).ToList()
+                select new SectionRange(range[0], range[1])
+            );
 }
 
 record SectionRange(int min, int max);
