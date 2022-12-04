b8998e165dd835bda30d4aff67312b87157b316c
Tom Johnston <thomas.johnston@softwire.com>
2022-12-04 12:22:27 +0000

    part 2

--- 2022/Day04/Solution.cs
+++ 2022/Day04/Solution.cs
@@ -27,11 +27,25 @@ class Solution : Solver
 
     public object PartTwo(string input)
     {
-        return 0;
+        var groups = GetGroupRanges(input);
+        return groups.Select(g =>
+        {
+            var gList = g.ToArray();
+            if (DoRangesOverlap(gList[0], gList[1]))
+            {
+                return 1;
+            }
+
+            return 0;
+        }).Sum();
     }
 
     bool IsRangeSubsetOfRange(SectionRange parentRange, SectionRange childRange) =>
         parentRange.max >= childRange.max && parentRange.min <= childRange.min;
+
+    bool DoRangesOverlap(SectionRange rangeA, SectionRange rangeB) =>
+        rangeA.max >= rangeB.min && rangeA.min <= rangeB.max
+        || rangeB.max >= rangeA.min && rangeB.min <= rangeA.max;
 
     IEnumerable<IEnumerable<SectionRange>> GetGroupRanges(string input) =>
         input.Split('\n')
