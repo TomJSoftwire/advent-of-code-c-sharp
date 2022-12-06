6eb61958f8ad2a3044db1bc3da86f6ddfafb4799
Tom Johnston <thomas.johnston@softwire.com>
2022-12-06 10:46:15 +0000

    part 2

--- 2022/Day06/Solution.cs
+++ 2022/Day06/Solution.cs
@@ -12,24 +12,24 @@ class Solution : Solver
 {
     public object PartOne(string input)
     {
-        return FindBufferEnd(input);
+        return FindBufferEnd(input, 4);
     }
 
     public object PartTwo(string input)
     {
-        return 0;
+        return FindBufferEnd(input, 14);
     }
 
-    int FindBufferEnd(string input)
+    int FindBufferEnd(string input, int uniqueCount)
     {
         var i = 0;
         while (i < input.Length)
         {
-            var window = input.Substring(i, 4);
+            var window = input.Substring(i, uniqueCount);
             var uniqueChars = window.Intersect(window).ToList();
-            if (uniqueChars.Count() == 4)
+            if (uniqueChars.Count() == uniqueCount)
             {
-                return i + 4;
+                return i + uniqueCount;
             }
 
             i++;
