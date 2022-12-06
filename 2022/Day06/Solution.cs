e83ee64bd1521f5790596d60cc1b4702677f19ab
Tom Johnston <thomas.johnston@softwire.com>
2022-12-06 10:45:01 +0000

    part 1

--- 2022/Day06/Solution.cs
+++ 2022/Day06/Solution.cs
@@ -7,14 +7,34 @@ using System.Text;
 
 namespace AdventOfCode.Y2022.Day06;
 
-[ProblemName("Tuning Trouble")]      
-class Solution : Solver {
+[ProblemName("Tuning Trouble")]
+class Solution : Solver
+{
+    public object PartOne(string input)
+    {
+        return FindBufferEnd(input);
+    }
 
-    public object PartOne(string input) {
+    public object PartTwo(string input)
+    {
         return 0;
     }
 
-    public object PartTwo(string input) {
-        return 0;
+    int FindBufferEnd(string input)
+    {
+        var i = 0;
+        while (i < input.Length)
+        {
+            var window = input.Substring(i, 4);
+            var uniqueChars = window.Intersect(window).ToList();
+            if (uniqueChars.Count() == 4)
+            {
+                return i + 4;
+            }
+
+            i++;
+        }
+
+        return -1;
     }
 }
