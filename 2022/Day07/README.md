df5b773f463485420e1d4d3d217e573b280635fa
Tom Johnston <thomas.johnston@softwire.com>
2022-12-07 10:58:17 +0000

    part 1

--- 2022/Day07/README.md
+++ 2022/Day07/README.md
@@ -97,3 +97,23 @@ To begin, find all of the directories with a total size of <em>at most 100000</e
 Find all of the directories with a total size of at most 100000. <em>What is the sum of the total sizes of those directories?</em>
 
 
+## --- Part Two ---
+Now, you're ready to choose a directory to delete.
+
+The total disk space available to the filesystem is <code><em>70000000</em></code>. To run the update, you need unused space of at least <code><em>30000000</em></code>. You need to find a directory you can delete that will <em>free up enough space</em> to run the update.
+
+In the example above, the total size of the outermost directory (and thus the total amount of used space) is <code>48381165</code>; this means that the size of the <em>unused</em> space must currently be <code>21618835</code>, which isn't quite the <code>30000000</code> required by the update. Therefore, the update still requires a directory with total size of at least <code>8381165</code> to be deleted before it can run.
+
+To achieve this, you have the following options:
+
+
+ - Delete directory <code>e</code>, which would increase unused space by <code>584</code>.
+ - Delete directory <code>a</code>, which would increase unused space by <code>94853</code>.
+ - Delete directory <code>d</code>, which would increase unused space by <code>24933642</code>.
+ - Delete directory <code>/</code>, which would increase unused space by <code>48381165</code>.
+
+Directories <code>e</code> and <code>a</code> are both too small; deleting them would not free up enough space. However, directories <code>d</code> and <code>/</code> are both big enough! Between these, choose the <em>smallest</em>: <code>d</code>, increasing unused space by <code><em>24933642</em></code>.
+
+Find the smallest directory that, if deleted, would free up enough space on the filesystem to run the update. <em>What is the total size of that directory?</em>
+
+
--- 2022/Day07/Solution.cs
+++ 2022/Day07/Solution.cs
@@ -7,14 +7,126 @@ using System.Text;
 
 namespace AdventOfCode.Y2022.Day07;
 
-[ProblemName("No Space Left On Device")]      
-class Solution : Solver {
+[ProblemName("No Space Left On Device")]
+class Solution : Solver
+{
+    public object PartOne(string input)
+    {
+        var storage = MapStorage(input);
+        var smallDirs = new List<int>();
+        GetDirectoriesSmallerThan(100000, storage, smallDirs);
+        return smallDirs.Sum();
+    }
 
-    public object PartOne(string input) {
+    public object PartTwo(string input)
+    {
         return 0;
     }
 
-    public object PartTwo(string input) {
-        return 0;
+    public void GetDirectoriesSmallerThan(int size, Directory directory, List<int> resultSizes)
+    {
+        var dirSize = directory.GetSize();
+        if (dirSize <= size)
+        {
+            resultSizes.Add(dirSize);
+        }
+
+        foreach (var child in directory.children)
+        {
+            GetDirectoriesSmallerThan(size, child, resultSizes);
+        }
+    }
+
+    Directory MapStorage(string input)
+    {
+        var instructions = input.Split("\n$ ");
+        var root = new Directory();
+        var currentDir = root;
+        foreach (var i in instructions)
+        {
+            var instructionAndOutput = i.Split('\n');
+            switch (instructionAndOutput[0].Substring(0, 2))
+            {
+                case "cd":
+                    currentDir = ChangeDirectory(currentDir, i);
+                    break;
+                case "ls":
+                    currentDir.AddContents(instructionAndOutput.Skip(1));
+                    break;
+                default: throw new Exception("Unsupported terminal command");
+            }
+        }
+
+        return root;
+    }
+
+
+    Directory ChangeDirectory(Directory curr, string instruction)
+    {
+        var path = instruction.Substring(3);
+        return path switch
+        {
+            "/" => curr.GetRoot(),
+            ".." => curr.parent,
+            _ => curr.GetChildDir(path),
+        };
     }
 }
+
+class Directory
+{
+    public Directory parent { set; get; }
+    public HashSet<Directory> children = new HashSet<Directory>();
+    public HashSet<File> files = new HashSet<File>();
+    public bool isRoot;
+    public string name { get; init; }
+
+    public Directory(Directory parent, string name)
+    {
+        this.parent = parent;
+        this.isRoot = false;
+        this.name = name;
+    }
+
+    public Directory()
+    {
+        this.isRoot = true;
+        this.name = "/";
+    }
+
+    public Directory GetChildDir(string name)
+        => children.Where(x => x.name == name).First();
+
+    public Directory GetRoot()
+    {
+        var curr = this;
+        while (this.parent != null)
+        {
+            curr = this.parent;
+        }
+
+        return curr;
+    }
+
+    public void AddContents(IEnumerable<string> list)
+    {
+        foreach (var item in list)
+        {
+            var arr = item.Split(' ');
+            if (arr[0] == "dir")
+            {
+                this.children.Add(new Directory(this, arr[1]));
+            }
+            else
+            {
+                var size = int.Parse(arr[0]);
+                this.files.Add(new File(size, arr[1]));
+            }
+        }
+    }
+
+    public int GetSize()
+        => this.children.Sum(child => child.GetSize()) + this.files.Sum(file => file.size);
+}
+
+record File(int size, string name);
